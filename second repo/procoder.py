b1437844ad05df432e34409a3c023a6c46cb9223
not-manishh <montyxcodes@gmail.com>
2025-01-15 17:59:07 +0530

    stage change

--- second repo/procoder.py
+++ second repo/procoder.py
@@ -1,3 +1,4 @@
 a=input("enter your name")
 b=input("enter your graduation year")
-print("The sum of both numbers is ",a+b)
+print("The sum of both numbers is ",a+b) 
+print(" after staged , this is a new change")
